a14c82880a561d5a4fb4436d48aeb05589d8344f
rsvzz <ronald_soft88@outlook.com>
2025-08-10 11:11:52 -0600

    added button in header

--- src/app.cpp
+++ src/app.cpp
@@ -17,6 +17,11 @@ static void activate(GtkApplication *app, gpointer user_data)
     GtkWidget *header = adw_header_bar_new();
 
     adw_header_bar_set_title_widget(ADW_HEADER_BAR(header), gtk_label_new("Convert SVG to PNG"));
+    // button for header
+    GtkWidget *w_open = gtk_button_new_from_icon_name("folder-symbolic");
+    adw_header_bar_pack_start(ADW_HEADER_BAR(header), w_open);
+    // end button for header
+    
     adw_toolbar_view_add_top_bar(ADW_TOOLBAR_VIEW(toolbar_view), header);
     gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
     adw_application_window_set_content(ADW_APPLICATION_WINDOW(window), GTK_WIDGET(toolbar_view));
