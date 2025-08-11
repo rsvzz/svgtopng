983a54aed4f586768e942cebd4240c06d2229166
rsvzz <ronald_soft88@outlook.com>
2025-08-11 10:18:05 -0600

    GtkFileDialog is Implement working

--- src/model_gtk/header_bar.cpp
+++ src/model_gtk/header_bar.cpp
@@ -1,6 +1,22 @@
 #include "../../inc/gtk4/header_bar.hpp"
 #include <glib-2.0/glib.h>
 
+static void on_folder_selected(GtkFileDialog *dialog, GAsyncResult *res, gpointer user_data) {
+    GError *error = nullptr;
+    GFile *folder = gtk_file_dialog_select_folder_finish(dialog, res, &error);
+
+    if (folder) {
+        HeaderBar *my_obj = static_cast<HeaderBar*>(user_data);
+        gchar *path = g_file_get_path(folder);
+        g_print("Carpeta seleccionada: %s\n", path);
+        g_free(path);
+        g_object_unref(folder);
+    } else {
+        g_warning("Error al seleccionar carpeta: %s", error->message);
+        g_error_free(error);
+    }
+}
+
 HeaderBar::HeaderBar(GtkWidget *_parent, GtkWidget *_header) : parent(_parent), header(_header)
 {
     adw_header_bar_set_title_widget(ADW_HEADER_BAR(header), gtk_label_new("Convert SVG to PNG"));
@@ -17,8 +33,9 @@ void HeaderBar::on_clicked_open_header_bar(GtkWidget *button, gpointer data)
 {
     auto my_obj = static_cast<HeaderBar*>(data); //catch gpointer
     if(GTK_IS_WINDOW(my_obj->get_parent())){
+         GtkFileDialog *dialog = gtk_file_dialog_new();
+         gtk_file_dialog_select_folder(GTK_FILE_DIALOG(dialog), my_obj->get_parent(), nullptr, GAsyncReadyCallback(on_folder_selected), static_cast<gpointer>(my_obj));
         
-        g_print("clicked open %s \n", GTK_WIDGET(my_obj->get_parent()));
     }
     
 }
